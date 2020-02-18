4348d1c66a472921a70aa12e043f101a2716ec12
Sandeep Bamane <sb@Sandeeps-Mac-mini.local>
2020-02-19 04:06:30 +0530

    first dumping ground

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# static-dump-files
+This is repo which holds all the static dump files which can be shared across
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+<li> 
+  <a href="./dump/service_only.war"  download>Camera Emporium </a> </li>
+
+      </ul>
+    </div>
+  );
+}
+
+export default App;
